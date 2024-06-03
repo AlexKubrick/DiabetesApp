c4ebee3dabc24b7472d87415888be37d08da3314
Alex Kubrick <alexandra.kamenchyk@mail.ru>
2024-06-04 00:51:43 +0700

    DrawerLayout + small changes

--- app/src/main/java/ru/alexkubrick/android/diabetesapp/presentation/main/MainActivity.kt
+++ app/src/main/java/ru/alexkubrick/android/diabetesapp/presentation/main/MainActivity.kt
@@ -5,55 +5,45 @@ import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.navigation.NavigationView
 import ru.alexkubrick.android.diabetesapp.presentation.dateDetail.DataDetailFragment
 import ru.alexkubrick.android.diabetesapp.R
+import ru.alexkubrick.android.diabetesapp.SettingsFragment
 import ru.alexkubrick.android.diabetesapp.databinding.ActivityMainBinding
-import ru.alexkubrick.android.diabetesapp.databinding.FragmentDataDetailBinding
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
     private lateinit var binding: ActivityMainBinding
+
     private val viewModel: JournalListViewModel by viewModels()
-//    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.navView.setNavigationItemSelectedListener(this)
-        val toggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar, R.string.open_menu, R.string.apply)
+        val toggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar.toolbar, R.string.open_menu, R.string.apply)
         binding.drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
         viewModel.state.observe(this) { state ->
             when (state) {
-                is State.Main -> {
-                    val mainFragment = JournalListFragment()
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.fragment_container, mainFragment)
-                        .commit()
-                }
-
-                is State.Edit -> {
-                    val detailFragment = DataDetailFragment.getInstance(dataId = state.dataId)
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.fragment_container, detailFragment)
-                        .addToBackStack(null)
-                        .commit()
-                }
+                is State.Main -> replaceFragment(fragment = JournalListFragment(), addToBackStack = false)
+                is State.Edit -> replaceFragment(fragment = DataDetailFragment.getInstance(dataId = state.dataId))
             }
         }
-//        binding.navView.setCheckedItem()
     }
 
-    private fun replaceFragment(fragment: Fragment) {
+    private fun replaceFragment(fragment: Fragment, fragmentName: String? = null, addToBackStack: Boolean = true) {
         val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.fragment_container, fragment)
+        transaction.replace(R.id.fragmentContainer, fragment)
+        if (addToBackStack) {
+            transaction.addToBackStack(fragmentName)
+        }
         transaction.commit()
     }
 
@@ -67,8 +57,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId) {
-//            R.id.
+            R.id.navSettings -> replaceFragment(SettingsFragment())
         }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
         return true
     }
 }
