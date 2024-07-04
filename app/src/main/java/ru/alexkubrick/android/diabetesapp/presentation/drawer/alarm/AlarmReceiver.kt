6591e039d9d43a9267ae1b38f2b188edaccb7ea2
Alex Kubrick <alexandra.kamenchyk@mail.ru>
2024-07-05 03:07:34 +0700

    Made alarm with push

--- app/src/main/java/ru/alexkubrick/android/diabetesapp/presentation/drawer/alarm/AlarmReceiver.kt
+++ app/src/main/java/ru/alexkubrick/android/diabetesapp/presentation/drawer/alarm/AlarmReceiver.kt
@@ -1,40 +1,48 @@
 package ru.alexkubrick.android.diabetesapp.presentation.drawer.alarm
 
-import android.Manifest.permission.POST_NOTIFICATIONS
-import android.app.Activity
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import androidx.core.app.ActivityCompat
+import android.os.Build
+import android.util.Log
 import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import ru.alexkubrick.android.diabetesapp.R
+import ru.alexkubrick.android.diabetesapp.presentation.main.MainActivity
 
 class AlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val message = intent.getStringExtra("key")
+        Log.d("My log", "alarm message: $message")
 
-    override fun onReceive(context: Context, intent: Intent?) {
-        displayNotification(context, intent)
+        sendNotification(context)
     }
 
-    fun displayNotification(context: Context, intent: Intent?) {
-        val intent = Intent(context, AlarmActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+    private fun sendNotification(context: Context) {
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val channelId = "alarm_channel"
+        val channelName = "Alarm Notifications"
+
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
         val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
 
-        // Code to display the notification
-        val builder = NotificationCompat.Builder(context, "alexKubrick")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val builder = NotificationCompat.Builder(context, channelId)
             .setSmallIcon(R.drawable.ic_alarm)
-            .setContentTitle("Diabetes Alarm Manager")
+            .setContentTitle("DiabetesApp alarm")
             .setContentText("Check the app")
-            .setAutoCancel(true)
-            .setDefaults(NotificationCompat.DEFAULT_ALL)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
-        builder.setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
 
-
-        val notificationManager = NotificationManagerCompat.from(context)
-        notificationManager.notify(123, builder.build())
+        notificationManager.notify(0, builder.build())
     }
 }
